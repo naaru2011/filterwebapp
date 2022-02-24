9d941f7ef5a7cc77e49bb1a24b0b95e572d82768
naaru2011 <83050539+naaru2011@users.noreply.github.com>
2022-02-23 17:39:54 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("haha.png");
+}
